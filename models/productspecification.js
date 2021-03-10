168b4d2af0b008ef69a56e86730ae88bc9d2b094
truyen.lv <truyen.lv@THTC188>
2021-03-10 15:26:51 +0700

    tao du lieu mau theo co

--- models/productspecification.js
+++ models/productspecification.js
@@ -1,25 +1,12 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class ProductSpecification extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      ProductSpecification.belongsTo(models.Product,{ foreignKey: 'productId' });
-      ProductSpecification.belongsTo(models.Specification ,{ foreignKey: 'specificationId' }); 
-    }
-  };
-  ProductSpecification.init({
-    description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'ProductSpecification',
-  });
-  return ProductSpecification;
+    const ProductSpecification = sequelize.define('ProductSpecification', {
+        description: DataTypes.TEXT
+    }, {});
+    ProductSpecification.associate = function(models) {
+        // associations can be defined here
+        ProductSpecification.belongsTo(models.Specification, { foreignKey: 'specificationId' });
+        ProductSpecification.belongsTo(models.Product, { foreignKey: 'productId' });
+    };
+    return ProductSpecification;
 };
