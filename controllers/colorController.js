3f8c25673c0d0ebb3de54acd9a46eebea734f506
truyen.lv <truyen.lv@THTC188>
2021-04-24 22:14:58 +0700

    backup

--- controllers/colorController.js
+++ controllers/colorController.js
@@ -4,19 +4,17 @@ let Color = models.Color;
 
 controller.getAll = (query) => {
     return new Promise((resolve, reject) => {
-        let options = {
+        let options={
+            attributes:['id','name','imagepath','code'],
             include: [{model: models.ProductColor,
-                include: [{
-                    model: models.Product,
-                    attributes: [],
-                    where:{}
-                }]
-            }],
-
-            attributes: ['id','name','imagepath','code']
-        }
-        if(query.category) {
-            options.include[0],include[0].where.categoryId = query.category;
+                include: [{model:models.Product, attributes: [],
+                            where: {  
+                            }
+                        }]
+                    }]
+        };
+        if(query.category>0){
+            options.include[0].include[0].where.categoryId=query.category;
         }
         Color
             .findAll(options)
